ccbfc94c9bbf71ecda30d13dc688aa6858083e8d
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-03-30 00:04:22 -0400

    added create delete and update dog views

---
+++ DogGo/Repositories/DogRepository.cs
@@ -0,0 +1,212 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using DogGo.Repositories;
+using DogGo.Controllers;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace DogGo.Repositories
+{
+    public class DogRepository : IDogRepository
+    {
+        private readonly IConfiguration _config;
+
+        public DogRepository (IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Dog> GetAllDogs()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Name, Id, OwnerId, Breed
+                                     FROM Dog";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Dog> dogs = new List<Dog>();
+
+                        while (reader.Read())
+                        {
+
+                            Dog doggy = new Dog()
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerID")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed"))
+                            };
+
+                            dogs.Add(doggy);
+
+                        }
+                        return dogs;
+                    }
+                }
+            }
+        }
+
+        public Dog GetDogById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, [Name], OwnerId, Breed
+                        FROM Dog
+                        WHERE Id = @id
+                    ";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Dog dog = new Dog
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerID"))
+
+                            };
+
+                            return dog;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+
+        public List<Dog> GetDogsByOwner(Owner ownerId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT d.Id as 'Id', d.[Name] as 'dogName', d.OwnerId, d.Breed, o.Name as 'ownerName'
+                        FROM Dog d
+                        JOIN Owner o ON o.Id = d.OwnerId
+                        WHERE o.Id = @id
+                       ";
+
+                    cmd.Parameters.AddWithValue("@id", ownerId);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Dog> dogs = new List<Dog>();
+                        while (reader.Read())
+                        {
+                            Dog dogg = new Dog()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("dogName")),
+                                Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                                OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId"))
+                            };
+
+                            dogs.Add(dogg);
+                        }
+
+                        return dogs;
+                        
+                    }
+                }
+            }
+        }
+
+        public void AddDog(Dog dog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Dog ([Name], Breed, OwnerId)
+                    OUTPUT INSERTED.ID
+                    VALUES (@name, @breed, @ownerId);
+                ";
+
+                    cmd.Parameters.AddWithValue("@name", dog.Name);
+                    cmd.Parameters.AddWithValue("@breed", dog.Breed);
+                    cmd.Parameters.AddWithValue("@ownerId", dog.OwnerId);
+
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    dog.Id = id;
+                }
+            }
+        }
+
+        public void UpdateDog(Dog dog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            UPDATE Dog
+                            SET 
+                                [Name] = @name, 
+                                Breed = @breed,
+                                OwnerId = @ownerId
+                            WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@name", dog.Name);
+                    cmd.Parameters.AddWithValue("@breed", dog.Breed);
+                    cmd.Parameters.AddWithValue("@ownerId", dog.OwnerId);
+                    cmd.Parameters.AddWithValue("@id", dog.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteDog(int dogId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Dog
+                            WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", dogId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
---
+++ DogGo/Repositories/IDogRepository.cs
@@ -0,0 +1,22 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using DogGo.Repositories;
+using DogGo.Controllers;
+
+namespace DogGo.Repositories
+{
+    public interface IDogRepository
+    {
+
+        List<Dog> GetAllDogs();
+        Dog GetDogById(int id);
+
+        public void AddDog(Dog dog);
+
+        public void UpdateDog(Dog dog);
+
+        public void DeleteDog(int id);
+
+    }
+}
